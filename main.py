ff897c7c7d6cefd9613f70c8754b8fd389e1323b
rockhover <rockhover@gmail.com>
2021-12-21 17:43:57 -0500

    Increased check threshold to 1mb; Added checks to only target 1 group chat

--- main.py
+++ main.py
@@ -13,26 +13,44 @@ load_dotenv()
 
 email = os.getenv('EMAIL')
 password = os.getenv('PASSWORD')
-target_id = ""
+target_name = "Patrick Day"
+# patrick's ID is the default
+target_id = "100003820518842"
 
-thread_id = "6566850490051694"
+pat = Image.open('stored_images/patwave.png')
+
+# Neo Kino Chat
+thrd_id = "2238564959531921"
+
+# test group chat
+# thrd_id = "6566850490051694"
 thread_type = ThreadType.GROUP
 
 async def fry(path):
     img = Image.open(path)
-    img = await deeppyer.deepfry(img)
-    img.save('stored_images/fry.jpg')
+    img = await deeppyer.deepfry(img, flares=True)
+    # pasting patrick onto the picture
+    global pat
+    ratio = min((img.size[0] / 2) / pat.size[0], (img.size[1] / 2) / pat.size[1])
+    new_height = ratio * pat.size[1]
+    new_width = ratio * pat.size[0]
+    pat = pat.resize((int(new_width), int(new_height)))
+    img.paste(pat, (0, int(img.size[1]) - int(new_height)), pat)
+    img.save('stored_images/merge.jpg')
 
 
 class EchoBot(Client):
     def onMessage(self, author_id, message_object, thread_id, thread_type, **kwargs):
         print("pingas")
         atts = message_object.attachments
-        if len(atts) != 0 and isinstance(atts[0], fbchat.ImageAttachment) and author_id != self.uid:
-            img_url = self.fetchImageUrl(atts[0].uid)
+        if thread_id == thrd_id and len(atts) != 0 and isinstance(atts[0], fbchat.ImageAttachment) and author_id != self.uid:
+            # img_url = self.fetchImageUrl(atts[0].uid)
+            img_url = atts[0].large_preview_url
+            print(img_url)
             r = requests.get(img_url, stream=True)
+            print(int(r.headers['Content-Length']))
             # Check if the image was retrieved successfully
-            if r.status_code == 200 and int(r.headers['Content-Length']) < 100000:
+            if r.status_code == 200: # and int(r.headers['Content-Length']) < 1000000:
                 # Set decode_content value to True, otherwise the downloaded image file's size will be zero.
                 r.raw.decode_content = True
                 # filename = img_url.split("/")[-1]
@@ -45,9 +63,9 @@ class EchoBot(Client):
                 loop = asyncio.get_event_loop()
                 loop.run_until_complete(fry("stored_images/penguin.jpg"))
 
-                print('[Image Fried]')
+                print('[Image Fried and Shopped]')
 
-                self.sendLocalImage('stored_images/fry.jpg', thread_id=thread_id, thread_type=thread_type)
+                self.sendLocalImage('stored_images/merge.jpg', thread_id=thrd_id, thread_type=thread_type)
                 print('[Image Sent]')
             else:
                 print('{Image Couldn\'t be retrieved}')
@@ -58,5 +76,12 @@ def main():
     client.listen()
     client.logout()
 
+
+def find_id_by_name():
+    client = Client(email, password)
+    # Fetches a list of all users you're currently chatting with, as `User` objects
+    return client.searchForUsers(target_name)[0].uid
+
 if __name__ == '__main__':
+    # find_id_by_name()
     main()
