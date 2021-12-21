96271ca31c822047436cc3095a5be652a1fee9d0
rockhover <rockhover@gmail.com>
2021-12-20 22:15:18 -0500

    Image Downloading Functionality

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 
 from itertools import islice
+
+import fbchat
 from fbchat import Client
 from fbchat.models import *
+import requests
+import shutil
 
 email = ""
 password = ""
@@ -9,6 +13,28 @@ target_id = ""
 
 thread_id = "6566850490051694"
 thread_type = ThreadType.GROUP
+
+class EchoBot(Client):
+    def onMessage(self, author_id, message_object, thread_id, thread_type, **kwargs):
+        print("pingas")
+        atts = message_object.attachments
+        if len(atts) != 0 and isinstance(atts[0], fbchat.ImageAttachment):
+            print("fat penguin")
+            img_url = self.fetchImageUrl(atts[0].uid)
+            r = requests.get(img_url, stream=True)
+            # Check if the image was retrieved successfully
+            if r.status_code == 200:
+                # Set decode_content value to True, otherwise the downloaded image file's size will be zero.
+                r.raw.decode_content = True
+                # filename = img_url.split("/")[-1]
+                # Open a local file with wb ( write binary ) permission.
+                with open("stored_images/penguin.jpg", 'wb') as f:
+                    shutil.copyfileobj(r.raw, f)
+
+                print('Image successfully Downloaded: Penguin')
+            else:
+                print('Image Couldn\'t be retrieved')
+
 
 def parseLogin():
     with open("personal_data.txt", "r") as f:
@@ -20,12 +46,8 @@ def parseLogin():
 
 def main():
 
-    client = Client(email, password)
-
-    print("Own id: {}".format(client.uid))
-
-    client.send(Message(text="Bot: Pingas"), thread_id=thread_id, thread_type=thread_type)
-
+    client = EchoBot(email, password)
+    client.listen()
     client.logout()
 
 if __name__ == '__main__':
